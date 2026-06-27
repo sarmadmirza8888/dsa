9c5c04d45dded187bbe0f8b315e009e08c622714
sarmadmirza8888 <sarmadmirza8888@gmail.com>
2026-06-27 23:26:15 +0500

    feat(heap):Add Brute solution for LastStoneWeight

--- heaps/brute/LastStoneWeightBrute.java
+++ heaps/brute/LastStoneWeightBrute.java
@@ -40,7 +40,9 @@ class LastStoneWeightBrute {
     }
     public static void main(String[] args) {
         LastStoneWeightBrute solution = new LastStoneWeightBrute();
+
         int[] stones = {2,7,4,1,8,1};
+
         System.out.println(solution.lastStoneWeight(stones));
     }
 }
